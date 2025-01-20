9136910927357a83ee23dbda4d22be4aeaaf0576
Ty Feng <bofeng1997@gmail.com>
2025-01-21 04:48:58 +0800

    ch16

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,36 @@ export const metadata: Metadata = {
   },
   description: "The official Next.js Learn Dashboard built with App Router.",
   metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+
+  // 关键词
+  keywords: ["nextjs", "react", "web development"],
+
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "nextjs project",
+    description: "Welcome ~",
+    url: "https://my-website.com",
+    siteName: "My Website",
+    images: [
+      {
+        url: "/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "My Website",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "My Website",
+    description: "Welcome to my website",
+    creator: "@username",
+    images: ["/opengraph-image.png"],
+  },
 };
 
 export default function RootLayout({
